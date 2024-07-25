8c0d4198fe18d6893e5de3953a3cf840ab21e66a
sreerajk06 <sreerajk06@gmail.com>
2024-07-25 12:41:09 -0400

    Update firstprogram.java

--- firstprogram.java
+++ firstprogram.java
@@ -1,8 +1,7 @@
 Class Demo
 {
-public static void main(String[] args)
-
-{
-System.out.println("Hello World");
-}
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World");
+  }
 }
